7648e98d836e2e5f9e2bf44099be6cc47940012d
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-24 16:39:31 -0300

    Implementar validação para o campo 'nome'

--- routes/middlewares/validations/UsuarioValid.js
+++ routes/middlewares/validations/UsuarioValid.js
@@ -3,7 +3,11 @@ const ExpressValidatorResultFormatted = require('../../../helpers/ExpressValidat
 class UsuarioValid {
 
     static validarCadastro(request, response, next) {
-        request.check('email').isEmail().withMessage('E-mail incorreto!');
+        request.check('email')
+            .isEmail().withMessage('E-mail incorreto!');
+        request.check('nome')
+            .notEmpty().withMessage('O campo "nome" é obrigatório, favor verificar.')
+            .len({min: 10, max: 45}).withMessage('O campo nome deve conter no mínimo 10 caracteres e no máximo 45.');
 
         request.getValidationResult()
             .then((resultado) => {
