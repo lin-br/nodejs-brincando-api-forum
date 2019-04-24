6f6293d6c156187dddd29ff3506320cf11f30471
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-24 16:43:23 -0300

    Implementar .trim() para os campos 'nome' e 'email'

--- routes/middlewares/validations/UsuarioValid.js
+++ routes/middlewares/validations/UsuarioValid.js
@@ -4,8 +4,10 @@ class UsuarioValid {
 
     static validarCadastro(request, response, next) {
         request.check('email')
+            .trim()
             .isEmail().withMessage('E-mail incorreto!');
         request.check('nome')
+            .trim()
             .notEmpty().withMessage('O campo "nome" é obrigatório, favor verificar.')
             .len({min: 10, max: 45}).withMessage('O campo nome deve conter no mínimo 10 caracteres e no máximo 45.');
 
