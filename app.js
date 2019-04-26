57520cfa8cca59ded19f03229b8a063ebbfb12d4
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-26 17:42:11 -0300

    Implementar rota para login no app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const UsuarioRoute = require('./routes/UsuarioRoute');
+const LoginRoute = require('./routes/LoginRoute');
 const expressValidator = require('express-validator');
 const ManipuladorErros = require('./routes/middlewares/ManipuladorDeErros');
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(expressValidator());
+app.use('/login', LoginRoute);
 app.use('/usuarios', UsuarioRoute);
 app.use(ManipuladorErros.deuRuim);
 
