68b481ace3259be782a0f41db8ce86a8730446c7
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-29 17:33:49 -0300

    Implementar na rota de login a validação de campos do formulário

--- routes/LoginRoute.js
+++ routes/LoginRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const LoginController = require('../controllers/LoginController');
+const LoginValid = require('./middlewares/validations/LoginValid');
 
-router.post('/', LoginController.realizarLogin);
+router.post('/', LoginValid.validarCampos, LoginValid.validarValoresDosCampos, LoginController.realizarLogin);
 
 module.exports = router;
