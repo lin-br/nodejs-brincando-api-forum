38e07faf37b55f5b211a7837860b5038c0201b24
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-29 17:34:39 -0300

    Incrementar campos no Entity Usuário após alteração do banco de dados

--- model/entity/UsuarioEntity.js
+++ model/entity/UsuarioEntity.js
@@ -1,9 +1,11 @@
 class UsuarioEntity {
 
-    constructor(nome, email, situacao = 1, dataCriacao = new Date(), dataAlteracao = null, dataExclusao = null) {
+    constructor(nome, email, senha, tipo, situacao = 1, dataCriacao = new Date(), dataAlteracao, dataExclusao) {
         this._nome = nome;
         this._email = email;
         this._situacao = situacao;
+        this._senha = senha;
+        this._tipo = tipo;
         this._dataCriacao = new Date(dataCriacao.getTime());
         this._dataAlteracao = dataAlteracao ? new Date(dataAlteracao.getTime()) : null;
         this._dataExclusao = dataExclusao ? new Date(dataExclusao.getTime()) : null;
@@ -32,6 +34,14 @@ class UsuarioEntity {
 
     get dataExclusao() {
         return new Date(this._dataExclusao.getTime());
+    }
+
+    get senha() {
+        return this._senha;
+    }
+
+    get tipo() {
+        return this._tipo;
     }
 }
 
