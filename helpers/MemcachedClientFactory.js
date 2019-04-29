c11a87e40e0e137a5eb511a0250f9a30b0ff049b
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-29 14:32:05 -0300

    Alterar configuração de cliente do Memcached
    - Estava com um período muito alto de resposta quando o serviço do,
    memcached não estava online. Diminui para 500 milissegundos.

--- helpers/MemcachedClientFactory.js
+++ helpers/MemcachedClientFactory.js
@@ -1,7 +1,14 @@
 const Memcached = require('memcached');
 
-Memcached.config.retries = 10;
-Memcached.config.retry = 10000;
+Memcached.config.retries = 5;
+Memcached.config.failures = 5;
+Memcached.config.retry = 500;
+Memcached.config.reconnect = 500;
+Memcached.config.timeout = 500;
+Memcached.config.idle = 500;
+Memcached.config.failuresTimeout = 1;
+Memcached.config.maxTimeout = 500;
+Memcached.config.minTimeout = 500;
 Memcached.config.remove = true;
 Memcached.config.poolSize = 10;
 
