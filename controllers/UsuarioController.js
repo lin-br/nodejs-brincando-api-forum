05945fc7cd9297182afb8d1de1b971aa0bd0b549
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-29 17:36:05 -0300

    Implementar criptografia de senha no cadastro de usuário

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,16 +1,25 @@
 const UsuarioRepository = require('../model/repository/UsuarioRepository');
 const UsuarioEntity = require('../model/entity/UsuarioEntity');
 const Url = require('../helpers/Url');
+const ManipuladorBcrypt = require('../helpers/ManipuladorBcrypt');
 
 class UsuarioController {
 
     static salvarUsuario(request, response) {
-        let {nome, email} = request.body;
-        let usuario = new UsuarioEntity(nome, email);
+        let {nome, email, senha, tipo} = request.body;
 
-        UsuarioRepository.cadastrar(usuario)
+        ManipuladorBcrypt.gerarHash(senha)
+            .then(hash => {
+                let usuario = new UsuarioEntity(nome, email, hash, tipo);
+
+                return UsuarioRepository.cadastrar(usuario);
+            })
             .then((id) => {
                 response.location(Url.montarUrlParaLocation(request, id)).send();
+            })
+            .catch(erro => {
+                console.log(erro);
+                response.status(500).json({erro: 'Ocorreu um erro com o servidor, favor tentar novamente mais tarde.'});
             });
     }
 }
