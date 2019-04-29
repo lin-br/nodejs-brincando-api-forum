5bfcd7cfd4bbb57279348d71d3fe23e42d2baa4b
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-29 17:36:50 -0300

    Implementar na rota de cadastro de usuário a validação de campos

--- routes/UsuarioRoute.js
+++ routes/UsuarioRoute.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const UsuarioController = require('../controllers/UsuarioController');
 const UsuarioValid = require('./middlewares/validations/UsuarioValid');
 
-router.post('/', UsuarioValid.validarCadastro, UsuarioController.salvarUsuario);
+router.post('/', UsuarioValid.validarCampos, UsuarioValid.validarValoresDosCampos, UsuarioController.salvarUsuario);
 
 module.exports = router;
