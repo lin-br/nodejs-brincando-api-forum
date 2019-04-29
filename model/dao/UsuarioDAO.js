d0dd3d05f3d68939e8ce76cec89d377aa5310dff
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-29 17:30:09 -0300

    Incrementar campos no DAO de Usuários após alteração do banco de dados

--- model/dao/UsuarioDAO.js
+++ model/dao/UsuarioDAO.js
@@ -6,8 +6,8 @@ class UsuariosDAO {
 
     salvar(usuario) {
         return new Promise((resolve, reject) => {
-            this._conexao.query('INSERT INTO usuarios(nome, email, situacao) VALUES (?,?,?);',
-                [usuario.nome, usuario.email, usuario.situacao],
+            this._conexao.query('INSERT INTO usuarios(nome, email, situacao, senha, id_tipo) VALUES (?,?,?,?,?);',
+                [usuario.nome, usuario.email, usuario.situacao, usuario.senha, usuario.tipo],
                 (erro, resultado) => {
                     if (erro) reject(erro);
                     else resolve(resultado.insertId);
